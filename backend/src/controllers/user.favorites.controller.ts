aa8f51b8aeaca947a2de9a9b6fa4339ca3a851de
Fernando Estevez <ferme1992@gmail.com>
2022-06-15 12:26:17 -0300

    Added remove favorite from user favorites

--- backend/src/controllers/user.favorites.controller.ts
+++ backend/src/controllers/user.favorites.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
-import { userInfo } from 'os';
-import { User, IUser } from '../models/User';
+import { User } from '../models/User';
 
 // @route   POST /favorites
 // @desc    Add favorite videos to user
@@ -27,6 +26,40 @@ export const addFavoriteVideo = async (req: Request, res: Response) => {
     await User.findByIdAndUpdate(user.id, user).exec();
 
     res.status(200).send('Video added to user favorites');
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Server Error');
+  }
+};
+
+export const removeFavoriteVideo = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.body.id).exec();
+
+    if (!user) {
+      return res.status(400).json({
+        errors: [{ msg: 'The user does not exists' }],
+      });
+    }
+
+    const videoId = req.body.videoId;
+
+    if (!videoId) {
+      return res.status(400).send('Video Id not provided');
+    }
+
+    if (!user.favoritedVideos.includes(videoId)) {
+      return res.status(400).send('User has not favorited this video');
+    }
+
+    const removeIndex = user.favoritedVideos.findIndex(() => {
+      (id: string) => id === videoId;
+    });
+    user.favoritedVideos.splice(removeIndex, 1);
+
+    await User.findByIdAndUpdate(user.id, user).exec();
+
+    res.status(200).send('Video removed from user favorites');
   } catch (err) {
     console.error(err);
     return res.status(500).send('Server Error');
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
 import passport from 'passport';
-import { addFavoriteVideo } from '../controllers/user.favorites.controller';
+import {
+  addFavoriteVideo,
+  removeFavoriteVideo,
+} from '../controllers/user.favorites.controller';
 
 const router = express.Router();
 const authJwt = passport.authenticate('jwt', { session: false });
 
 router.post('/addFavorite', authJwt, addFavoriteVideo);
+router.post('/removeFavorite', authJwt, removeFavoriteVideo);
 
 export default router;
