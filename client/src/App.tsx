1bca06db0c7d199578c96af9754de43e41097f63
Fernando Estevez <ferme1992@gmail.com>
2022-06-16 21:29:18 -0300

    Partial signup form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Landing from './components/layout/Landing';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/signIn' element={<SignIn />} />
+          <Route path='/signUp' element={<SignUp />} />
         </Routes>
       </Router>
     </AuthProvider>
---
+++ client/src/components/auth/SignUp.tsx
@@ -0,0 +1,130 @@
+import React, { FormEvent } from 'react';
+import {
+  Container,
+  Box,
+  TextField,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Button,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { Visibility, VisibilityOff, AccountCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useAuth from '../../context/AuthContext';
+
+interface State {
+  password: string;
+  showPassword: boolean;
+}
+
+const SignUp = () => {
+  const { signUpUser } = useAuth();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    signUpUser(
+      formData.get('name') as string,
+      formData.get('email') as string,
+      formData.get('password') as string
+    );
+  }
+
+  const [values, setValues] = React.useState<State>({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Container>
+      <Box component='h1'>Sign Up</Box>
+      <Box component='p'>
+        <Stack direction='row' spacing={1}>
+          <AccountCircle fontSize='small' />
+          <Typography variant='h2' sx={{ fontSize: '1rem', mb: '1rem' }}>
+            Create your Account
+          </Typography>
+        </Stack>
+      </Box>
+      <Box component='form' onSubmit={handleSubmit}>
+        <Box>
+          <TextField
+            type='text'
+            required
+            margin='normal'
+            placeholder='Name'
+            name='name'
+          />
+        </Box>
+        <Box>
+          <TextField
+            required
+            id='email-input'
+            label='Email'
+            margin='normal'
+            name='email'
+          />
+          <InputLabel htmlFor='outlined-adornment-password'>
+            Password
+          </InputLabel>
+          <OutlinedInput
+            required
+            id='outlined-adornment-password'
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            name='password'
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge='end'
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label='Password'
+          />
+        </Box>
+        <Button
+          type='submit'
+          variant='contained'
+          aria-label='sign up button'
+          sx={{ my: '1.5rem' }}
+        >
+          Sign Up
+        </Button>
+      </Box>
+      <Box component='p'>
+        Already have an account? <Link to='/signIn'>Sign In</Link>
+      </Box>
+    </Container>
+  );
+};
+
+export default SignUp;
