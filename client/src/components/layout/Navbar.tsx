5102e0681a98a954d1569c332cd3ee86dd40abc8
Fernando Estevez <ferme1992@gmail.com>
2022-06-21 16:19:28 -0300

    Navbar styling

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Typography, Stack, AppBar } from '@mui/material';
+import { Typography, Stack, AppBar, Box, Toolbar } from '@mui/material';
 import {
   Logout,
   ElectricBolt,
@@ -18,45 +18,45 @@ const Navbar = () => {
       navigate('/');
     }
   }, [loggedIn]);
-  // TODO add search bar, fav, style
   const authLinks = (
-    <Stack direction='column' spacing={1}>
-      <Stack direction='row' spacing={1} onClick={logout}>
-        <Logout fontSize='small' />
-        <Typography component='a' sx={{ fontSize: '1rem', mb: '1rem' }}>
-          Logout
-        </Typography>
-      </Stack>
+    <Stack direction='row' spacing={1} onClick={logout}>
+      <Logout fontSize='small' />
+      <Typography sx={{ fontSize: '1rem', mb: '1rem' }}>Logout</Typography>
     </Stack>
   );
   const guestLinks = (
-    <Stack direction='column' spacing={1}>
+    <>
       <Stack direction='row' spacing={1} component={Link} to='/signIn'>
         <Login fontSize='small' />
-        <Typography component='a' sx={{ fontSize: '1rem', mb: '1rem' }}>
-          Sign In
-        </Typography>
+        <Typography sx={{ fontSize: '1rem', mb: '1rem' }}>Sign In</Typography>
       </Stack>
       <Stack direction='row' spacing={1} component={Link} to='/signUp'>
         <AccountCircle fontSize='small' />
-        <Typography component='a' sx={{ fontSize: '1rem', mb: '1rem' }}>
-          Sign Up
-        </Typography>
+        <Typography sx={{ fontSize: '1rem', mb: '1rem' }}>Sign Up</Typography>
       </Stack>
-    </Stack>
+    </>
   );
 
   return (
-    //Add navbar properties, style https://mui.com/material-ui/react-app-bar/#main-content
-    <AppBar position='static'>
-      <Stack direction='row' spacing={1} component={Link} to='/'>
-        <ElectricBolt fontSize='small' />
-        <Typography component='a' sx={{ fontSize: '1rem', mb: '1rem' }}>
-          LightTube
-        </Typography>
-      </Stack>
-      <>{loggedIn ? authLinks : guestLinks}</>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Stack
+            direction='row'
+            spacing={1}
+            component={Link}
+            to='/'
+            sx={{ flexGrow: 1 }}
+          >
+            <ElectricBolt fontSize='small' />
+            <Typography sx={{ fontSize: '1rem', mb: '1rem' }}>
+              LightTube
+            </Typography>
+          </Stack>
+          {loggedIn ? authLinks : guestLinks}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
