f6755e9fc869fef3cb731219877f747e3745b112
Fernando Estevez <ferme1992@gmail.com>
2022-06-21 18:06:44 -0300

    Dockerized app

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
 export default {
   jwtSecret: process.env.JWT_SECRET || 'mysecrettoken',
   DB: {
-    URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/lighttube',
+    URI: process.env.MONGODB_URI || 'mongodb://mongo:27017/lighttube',
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD,
   },
